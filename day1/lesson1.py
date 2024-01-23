ca14fd4207473f6db94e5e2be5471ff710a357ed
nokukuno16 <nokudvali@gmail.com>
2024-01-23 22:44:23 +0400

    second commit

---
+++ day1/lesson1.py
@@ -0,0 +1,18 @@
+name = "nikolas"
+surname ="dvali"
+age = 13
+height = "me ra vici"
+dont_knows_programming = True
+is_ugly = False
+
+#print(name + " " + surname)
+#print(name + " " + str(age))
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(dont_knows_programming))
+
+
+print("me var " + name + surname + " da var " + str(age))
